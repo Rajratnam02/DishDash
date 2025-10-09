fcaf17f6528bca1eda3013fbc9eacae2c72b7369
Rajratnam02 <rajratnam2005@gmail.com>
2025-10-09 20:14:05 +0530

    Made minor changes

--- src/components/IngredientDiv.jsx
+++ src/components/IngredientDiv.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 const IngredientDiv = (props) => {
   return (
-    <div className='w-full py-4 border-b  flex justify-between px-4'>
+    <div className='w-full py-4 border-b text-lg font-medium flex justify-between px-4'>
         <p>{props.product}</p>
         <p>{props.quantity}</p>
     </div>
--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -10,7 +10,7 @@ const NavBar = () => {
   return (
       <div className=' mx-5 mr-5 px-5 py-3 flex justify-between mt-5 shadow-[0px_0px_5px_5px_rgba(82,87,94,0.2)] rounded-2xl items-center '>
           
-          <div className='flex items-center h-10'>
+          <div onClick={()=>{navigate("/")}} className='flex items-center cursor-pointer h-10'>
               <p className=' text-3xl  lora-medium text-zinc-950'>Dish<span className='text-red-600'>Dash</span></p>
               <img className='h-full -mx-2 ' src="/src/assets/WhatsApp_Image_2025-09-14_at_22.39.23_5488428c-removebg-preview.png" alt="" />
           </div>
