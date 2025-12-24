11b8ba23a92ce92c3e7dc89de8010bdfd1ba7b89
Rajratnam02 <rajratnam2005@gmail.com>
2025-12-24 21:15:53 +0530

    feat: implement warm glow radial background styling

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,15 @@
 import React from 'react'
 import Navbar from './components/Navbar'
+import { Route, Routes } from 'react-router-dom'
+import Homepage from './pages/Homepage'
+import Warmglow from './components/Warmglow'
 
 const App = () => {
   return (
     <div className='h-screen w-screen bg-stone-950'>
+      <Warmglow />
       <Navbar />
+      
     </div>
   )
 }
---
+++ src/components/Warmglow.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Warmglow = () => {
+  return (
+    <div className='fixed inset-0 z-0 bg-[radial-gradient(circle_at_50%_-20%,rgba(245,158,11,0.12)_0%,transparent_50%),radial-gradient(circle_at_0%_100%,rgba(16,185,129,0.05)_0%,transparent_40%)] text-white  '>
+        
+    </div>
+  )
+}
+
+export default Warmglow
