d7789d757e16fa61b5465385d8933ee23334ac62
Rajratnam02 <rajratnam2005@gmail.com>
2025-12-26 19:59:35 +0530

    Fixed worm glow

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -9,7 +9,7 @@ const Navbar = () => {
   return (
     <div className=' fixed bottom-10 z-100 left-1/2 -translate-x-1/2'>
         <div className='bg-stone-900/80 backdrop-blur-2xl border border-white/5 rounded-full px-4 py-2 flex items-center gap-2 shadow-2xl'>
-            <button onClick={()=>(navigate("/"))} className='bg-amber-500 text-stone-950 p-3 rounded-full hover:scale-110 transition shadow-lg shadow-amber-500/20'>
+            <button onClick={()=>(navigate("/"))} className='bg-amber-500 cursor-pointer text-stone-950 p-3 rounded-full hover:scale-110 transition shadow-lg shadow-amber-500/20'>
                 <svg width="20" height="20" fill="currentColor" viewBox="0 0 24 24"><path d="M10 20v-6h4v6h5v-8h3L12 3 2 12h3v8z"/></svg>
             </button>
             <p className='text-stone-500 px-6 py-2 text-[10px] font-black uppercase tracking-[0.4em]'>{location.pathname.split("/")[1] || "Home"}</p>
--- src/components/Warmglow.jsx
+++ src/components/Warmglow.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 const Warmglow = () => {
   return (
-    <div className='fixed inset-0 z-[-1] bg-[radial-gradient(circle_at_50%_-20%,rgba(245,158,11,0.12)_0%,transparent_50%),radial-gradient(circle_at_0%_100%,rgba(16,185,129,0.05)_0%,transparent_40%)] text-white  '>
+    <div className='fixed inset-0 pointer-events-none bg-[radial-gradient(circle_at_50%_-20%,rgba(245,158,11,0.12)_0%,transparent_50%),radial-gradient(circle_at_0%_100%,rgba(16,185,129,0.05)_0%,transparent_40%)] '>
         
     </div>
   )
